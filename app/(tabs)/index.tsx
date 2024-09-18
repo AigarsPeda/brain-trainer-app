ceef0553a7a08f041ea782b6dc1355751f571df3
Aigars <aigarspeda@gmail.com>
2024-09-18 12:13:01 +0300

    Add missing files and configurations

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -20,7 +20,7 @@ export default function HomeScreen() {
       }
     >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Welcome! 1</ThemedText>
         <HelloWave />
       </ThemedView>
       <Link href="/adScreen" asChild>
