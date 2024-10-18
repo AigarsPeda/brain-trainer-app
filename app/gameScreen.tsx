b6e301eda32b6b7a51df0679291ae8f5e51c67e4
A.Peda <aigarspeda@gmail.com>
2024-10-18 19:49:03 +0300

    Refactor game screen to remove unused imports and commented code

--- app/gameScreen.tsx
+++ app/gameScreen.tsx
@@ -1,20 +1,20 @@
 import MathTaskWithResult from "@/components/mathTasks/MathTaskWithResult";
-import { ThemedText } from "@/components/ThemedText";
+// import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import * as Device from "expo-device";
+// import * as Device from "expo-device";
 import { useLocalSearchParams, useRouter } from "expo-router";
-import { RewardedAd, TestIds } from "react-native-google-mobile-ads";
+// import { RewardedAd, TestIds } from "react-native-google-mobile-ads";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const ios = "ca-app-pub-5238286944896076/6557213296";
-const android = "ca-app-pub-5238286944896076/2318585385";
+// const ios = "ca-app-pub-5238286944896076/6557213296";
+// const android = "ca-app-pub-5238286944896076/2318585385";
 
-const adDeviceId = Device.osName === "iOS" ? ios : android;
-const adUnitId = __DEV__ ? TestIds.REWARDED : adDeviceId;
+// const adDeviceId = Device.osName === "iOS" ? ios : android;
+// const adUnitId = __DEV__ ? TestIds.REWARDED : adDeviceId;
 
-const rewarded = RewardedAd.createForAdRequest(adUnitId, {
-  keywords: ["games", "kids", "fun", "education", "learning"],
-});
+// const rewarded = RewardedAd.createForAdRequest(adUnitId, {
+//   keywords: ["games", "kids", "fun", "education", "learning"],
+// });
 
 export default function AdScreen() {
   const router = useRouter();
