9e81cb94cda6dd1a32c249f5a23a2534c5e8a3f4
Aigars <aigarspeda@gmail.com>
2025-04-13 20:23:13 +0300

    feat: add gorhom/bottom-sheet dependency and update package files

--- app/game/[level].tsx
+++ app/game/[level].tsx
@@ -6,21 +6,23 @@ import Progressbar from "@/components/Progressbar";
 import { StatisticsItem } from "@/components/StatisticsItem";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import { ALL_TASKS, LevelsEnum } from "@/context/app.context.reducer";
+import { ALL_TASKS, LevelsEnum, TaskAnswerType, TaskOptionType } from "@/context/app.context.reducer";
 import useAppContext from "@/hooks/useAppContext";
+import { useThemeColor } from "@/hooks/useThemeColor";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 import { useLocalSearchParams, useRouter } from "expo-router";
+import { useCallback, useEffect, useMemo, useRef } from "react";
+import { StyleSheet } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-
-// const { width } = Dimensions.get("window");
-// const WIDOW_WIDTH_WITH_MARGIN = width - 32;
 
 export default function GameLevelScreen() {
   const {
     dispatch,
     state: {
+      lives,
       results,
       availableLevels,
-      lives,
       game: { currentTaskInLevel },
     },
   } = useAppContext();
@@ -82,6 +84,44 @@ export default function GameLevelScreen() {
       type: "GET_NEXT_TASK_IN_LEVEL",
     });
   };
+
+  const getCountOfCorrectAnswersAndWrong = (options: TaskOptionType[]) => {
+    const currentTaskCorrectAnswers = options.filter((o) => o.isCorrect).length;
+    const currentTaskWrongAnswers = options.filter((o) => !o.isCorrect).length;
+
+    return {
+      currentTaskWrongAnswers,
+      currentTaskCorrectAnswers,
+    };
+  };
+
+  const getCountOfLevelCorrectAnswersAndWrong = (answers: TaskAnswerType[]) => {
+    const levelAnswerCorrectAnswers = answers.filter((o) => o.isCorrect).length;
+    const levelAnswerWrongAnswers = answers.filter((o) => !o.isCorrect).length;
+
+    return {
+      levelAnswerWrongAnswers,
+      levelAnswerCorrectAnswers,
+    };
+  };
+
+  const { currentTaskCorrectAnswers, currentTaskWrongAnswers } = getCountOfCorrectAnswersAndWrong(
+    currentTask?.options || []
+  );
+
+  const { levelAnswerCorrectAnswers, levelAnswerWrongAnswers } = getCountOfLevelCorrectAnswersAndWrong(
+    levelAnswer?.answers || []
+  );
+
+  console.log("currentTaskCorrectAnswers", currentTaskCorrectAnswers);
+  console.log("currentTaskWrongAnswers", currentTaskWrongAnswers);
+  console.log("levelAnswerCorrectAnswers", levelAnswerCorrectAnswers);
+  console.log("levelAnswerWrongAnswers", levelAnswerWrongAnswers);
+
+  const isAllAnswersCorrect =
+    currentTaskCorrectAnswers === levelAnswer?.answers.length && levelAnswerWrongAnswers === 0;
+
+  console.log("isAllAnswersCorrect", isAllAnswersCorrect);
 
   if (!levelTasks || levelTasks.length === 0) {
     console.error("No tasks found for level", level);
@@ -161,41 +201,126 @@ export default function GameLevelScreen() {
             justifyContent: "center",
           }}
         >
-          <MainButton
-            onPress={() => {
-              if (!isTaskChecked) {
-                dispatch({
-                  type: "CHECK_ANSWERS",
-                  payload: {
-                    level,
-                    currentTaskNumber: currentTaskInLevel,
-                  },
-                });
+          {!isTaskChecked && (
+            <MainButton
+              onPress={() => {
+                if (!isTaskChecked) {
+                  dispatch({
+                    type: "CHECK_ANSWERS",
+                    payload: {
+                      level,
+                      currentTaskNumber: currentTaskInLevel,
+                    },
+                  });
 
-                return;
-              }
+                  return;
+                }
 
-              if (isTaskChecked && maxLevelStep === currentTaskInLevel) {
-                goToNextLevel();
-                return;
-              }
+                if (isTaskChecked && maxLevelStep === currentTaskInLevel) {
+                  goToNextLevel();
+                  return;
+                }
 
-              getNextTaskInLevel();
-            }}
-            disabled={!isAtLeastOneTaskAnswered}
-          >
-            <ThemedText
-              type="defaultSemiBold"
-              style={{
-                fontSize: 24,
-                // color: isAtLeastOneTaskAnswered ? "#1f2937" : "#9ca3af",
+                getNextTaskInLevel();
               }}
+              disabled={!isAtLeastOneTaskAnswered}
             >
-              {isTaskChecked ? "Nākamais uzdevums" : "Pārbaudīt"}
-            </ThemedText>
-          </MainButton>
+              <ThemedText
+                type="defaultSemiBold"
+                style={{
+                  fontSize: 20,
+                }}
+              >
+                {isTaskChecked ? "Nākamais uzdevums" : "Pārbaudīt"}
+              </ThemedText>
+            </MainButton>
+          )}
         </ThemedView>
       </ThemedView>
+
+      {/* Render ShowResults when isTaskChecked is true */}
+      {isTaskChecked && <ShowResults isAllAnswersCorrect={isAllAnswersCorrect} onNextTaskPress={goToNextLevel} />}
     </ThemedView>
   );
 }
+
+interface ShowResultsProps {
+  isAllAnswersCorrect: boolean;
+  onNextTaskPress: () => void;
+}
+
+function ShowResults({ onNextTaskPress, isAllAnswersCorrect }: ShowResultsProps) {
+  const sheetRef = useRef<BottomSheet>(null);
+  const { background } = useThemeColor();
+
+  const snapPoints = useMemo(() => ["35%"], []);
+
+  const handleSheetChange = useCallback((index: number) => {
+    console.log("handleSheetChange", index);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      sheetRef.current?.snapToIndex(0);
+    }, 100);
+  }, []);
+
+  // This will hide the handle indicator at the top of the bottom sheet
+  const EmptyHandle = () => <></>;
+
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <BottomSheet
+        index={0}
+        ref={sheetRef}
+        snapPoints={snapPoints}
+        enableDynamicSizing={false}
+        onChange={handleSheetChange}
+        handleComponent={EmptyHandle}
+        handleStyle={{ height: 0 }}
+      >
+        <BottomSheetView style={{ ...styles.contentContainer, backgroundColor: background }}>
+          <ThemedView>
+            <ThemedText type="subtitle" style={{ marginBottom: 10, textAlign: "left" }}>
+              {isAllAnswersCorrect ? "Pareizi! 🎉" : "Uzdevums pabeigts! 🔥"}
+            </ThemedText>
+            <ThemedText style={{ fontSize: 16, marginBottom: 20, textAlign: "left" }}>
+              {isAllAnswersCorrect
+                ? "Visas atbildes ir pareizas! Turpini tā!"
+                : "Daļa no atbildēm nav pareiza. Nākamreiz būs labāk!"}
+            </ThemedText>
+            <ThemedView style={{ width: "100%", alignItems: "center" }}>
+              <MainButton onPress={onNextTaskPress}>
+                <ThemedText
+                  type="defaultSemiBold"
+                  style={{
+                    fontSize: 20,
+                  }}
+                >
+                  Nākamais uzdevums
+                </ThemedText>
+              </MainButton>
+            </ThemedView>
+          </ThemedView>
+        </BottomSheetView>
+      </BottomSheet>
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 1000,
+    position: "absolute",
+    backgroundColor: "rgba(0, 0, 0, 0.3)",
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 36,
+  },
+});
